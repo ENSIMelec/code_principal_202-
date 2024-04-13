3ab3472c164a775fcf0aa8db0fd16835f3d2919b
Estebancott <estebancott@gmail.com>
2024-04-13 12:31:43 +0200

    Asserv

---
+++ Asserv.py
@@ -0,0 +1,181 @@
+import serial
+import threading
+
+class Asserv:
+    def __init__(self, port='/dev/ttyUSB0', baudrate=115200, buffer_size=1024):
+        self.buffer_size = buffer_size
+        self.encGauche = [None] * buffer_size
+        self.index_encGauche = 0
+        self.encDroit = [None] * buffer_size
+        self.index_encDroit = 0
+        self.vitesse_G = [None] * buffer_size
+        self.index_vitesse_G = 0
+        self.vitesse_D = [None] * buffer_size
+        self.index_vitesse_D = 0
+        self.Output_PID_vitesse_G = [None] * buffer_size
+        self.index_Output_PID_vitesse_G = 0
+        self.Output_PID_vitesse_D = [None] * buffer_size
+        self.index_Output_PID_vitesse_D = 0
+        self.cmd_vitesse_G = [None] * buffer_size
+        self.index_cmd_vitesse_G = 0
+        self.cmd_vitesse_D = [None] * buffer_size
+        self.index_cmd_vitesse_D = 0
+        self.angle = [None] * buffer_size
+        self.index_angle = 0
+        self.Output_PID_angle = [None] * buffer_size
+        self.index_Output_PID_angle = 0
+        self.cmd_angle = [None] * buffer_size
+        self.index_cmd_angle = 0
+        self.distance = [None] * buffer_size
+        self.index_distance = 0
+        self.Output_PID_distance = [None] * buffer_size
+        self.index_Output_PID_distance = 0
+        self.cmd_distance = [None] * buffer_size
+        self.index_cmd_distance = 0
+        self.angle_ok = False
+        self.distance_ok = False
+        self.x = [None] * buffer_size
+        self.index_x = 0
+        self.y = [None] * buffer_size
+        self.index_y = 0
+        self.serial = serial.Serial(port, baudrate)
+        self.thread = threading.Thread(target=self.receive_data)
+        self.thread.daemon = True
+        self.thread.start()
+    
+    def goto(self, x, y):
+        command = f"asserv goto {x} {y}\n"
+        self.serial.write(command.encode())
+        while (self.distance_ok == 0):
+            pass
+        return True
+    
+    def rotate(self, x, y):
+        command = f"asserv rotate {x} {y}\n"
+        self.serial.write(command.encode())
+        while (self.angle_ok == 0):
+            pass
+        return True
+    
+    def receive_data(self):
+        while True:
+            data = self.serial.readline().decode().strip()
+            if data.startswith("A"): # Valeur du codeur Gauche
+                _, x = data.split()
+                self.encGauche[self.index_encGauche] = int(x)
+                self.index_encGauche = (self.index_encGauche + 1) % self.buffer_size
+            elif data.startswith("B"): # Valeur du codeur Droit
+                _, x = data.split()
+                self.encDroit[self.index_encDroit] = int(x)
+                self.index_encDroit = (self.index_encDroit + 1) % self.buffer_size
+            elif data.startswith("C"): # Vitesse réel moteur Gauche
+                _, x = data.split()
+                self.vitesse_G[self.index_vitesse_G] = float(x)
+                self.index_vitesse_G = (self.index_vitesse_G + 1) % self.buffer_size
+            elif data.startswith("D"): # Vitesse réel moteur Droit
+                _, x = data.split()
+                self.vitesse_D[self.index_vitesse_D] = float(x)
+                self.index_vitesse_D = (self.index_vitesse_D + 1) % self.buffer_size
+            elif data.startswith("E"): # Sortie du PID vitesse moteur Gauche
+                _, x = data.split()
+                self.Output_PID_vitesse_G[self.index_Output_PID_vitesse_G] = float(x)
+                self.index_Output_PID_vitesse_G = (self.index_Output_PID_vitesse_G + 1) % self.buffer_size
+            elif data.startswith("F"): # Sortie du PID vitesse moteur Droit
+                _, x = data.split()
+                self.Output_PID_vitesse_D[self.index_Output_PID_vitesse_D] = float(x)
+                self.index_Output_PID_vitesse_D = (self.index_Output_PID_vitesse_D + 1) % self.buffer_size
+            elif data.startswith("G"): # Consigne de vitesse moteur Gauche
+                _, x = data.split()
+                self.cmd_vitesse_G[self.index_cmd_vitesse_G] = float(x)
+                self.index_cmd_vitesse_G = (self.index_cmd_vitesse_G + 1) % self.buffer_size
+            elif data.startswith("H"): # Consigne de vitesse moteur Droit
+                _, x = data.split()
+                self.cmd_vitesse_D[self.index_cmd_vitesse_D] = float(x)
+                self.index_cmd_vitesse_D = (self.index_cmd_vitesse_D + 1) % self.buffer_size
+            elif data.startswith("I"): # angle mesurer
+                _, x = data.split()
+                self.angle[self.index_angle] = float(x)
+                self.index_angle = (self.index_angle + 1) % self.buffer_size
+            elif data.startswith("J"): # angle PID
+                _, x = data.split()
+                self.Output_PID_angle[self.index_Output_PID_angle] = float(x)
+                self.index_Output_PID_angle = (self.index_Output_PID_angle + 1) % self.buffer_size
+            elif data.startswith("K"): # cmd angle
+                _, x = data.split()
+                self.cmd_angle[self.index_cmd_angle] = float(x)
+                self.index_cmd_angle = (self.index_cmd_angle + 1) % self.buffer_size
+            elif data.startswith("L"): # distance mesurer
+                _, x = data.split()
+                self.distance[self.index_distance] = float(x)
+                self.index_distance = (self.index_distance + 1) % self.buffer_size
+            elif data.startswith("M"): # distance PID
+                _, x = data.split()
+                self.Output_PID_distance[self.index_Output_PID_distance] = float(x)
+                self.index_Output_PID_distance = (self.index_Output_PID_distance + 1) % self.buffer_size
+            elif data.startswith("O"): # cmd distance
+                _, x = data.split()
+                self.cmd_distance[self.index_cmd_distance] = float(x)
+                self.index_cmd_distance = (self.index_cmd_distance + 1) % self.buffer_size
+            elif data.startswith("P"): # angle ok
+                _, x = data.split()
+                self.angle_ok = bool(x)
+            elif data.startswith("Q"): # distance ok 
+                _, x = data.split()
+                self.distance_ok = bool(x)
+            elif data.startswith("X"): # position x
+                _, x = data.split()
+                self.x[self.index_x] = float(x)
+                self.index_x = (self.index_x + 1) % self.buffer_size
+            elif data.startswith("Y"): # position y
+                _, x = data.split()
+                self.y[self.index_y] = float(x)
+                self.index_y = (self.index_y + 1) % self.buffer_size
+    
+# 
+# /*************************************/
+# /*******Envoie des données************/
+# /*************************************/
+# void sendData()
+# {
+	# Serial.print("A"); // Valeur du codeur Gauche
+	# Serial.println(last_encGauche);
+	# Serial.print("B"); // Valeur du codeur Droit
+	# Serial.println(last_encDroit);
+	# Serial.print("C"); // Vitesse réel moteur Gauche
+	# Serial.println(vitesse_G, 5);
+	# Serial.print("D"); // Vitesse réel moteur Droit
+	# Serial.println(vitesse_D, 5);
+	# Serial.print("E"); // Sortie du PID vitesse moteur Gauche
+	# Serial.println(Output_PID_vitesse_G, 5);
+	# Serial.print("F"); // Sortie du PID vitesse moteur Droit
+	# Serial.println(Output_PID_vitesse_D, 5);
+	# Serial.print("G"); // Consigne de vitesse moteur Gauche
+	# Serial.println(cmd_vitesse_G, 5);
+	# Serial.print("H"); // Consigne de vitesse moteur Droit
+	# Serial.println(cmd_vitesse_D, 5);
+	# Serial.print("I"); // angle mesurer
+	# Serial.println(angle, 5);
+	# Serial.print("J"); // angle PID
+	# Serial.println(Output_PID_angle);
+	# Serial.print("K");
+	# Serial.println(cmd_angle, 5);
+	# Serial.print("L"); // distance mesurer
+	# Serial.println(distance, 5);
+	# Serial.print("M"); // distance PID
+	# Serial.println(Output_PID_distance, 5);
+	# Serial.print("O"); // cmd distance
+	# Serial.println(cmd_distance, 5);
+	# Serial.print("P"); // angle ok
+	# Serial.println(angle_ok);
+	# Serial.print("Q"); // distance ok
+	# Serial.println(distance_ok);
+	# Serial.print("X"); // position x
+	# Serial.println(x);
+	# Serial.print("Y"); // position y
+	# Serial.println(y);
+# 
+	# Update_IT = false;
+# }
+# /*************************************/
+# /*************************************/
+# /*************************************/
